fb1a0788d4e46d8bb15b5db0fad54ccea54440cc
Ariunbayar Baterdene <abaterd@odesk.com>
2013-10-04 12:26:17 +0800

    n/a: App with product

--- src/Qanda/HelloBundle/Controller/DefaultController.php
+++ src/Qanda/HelloBundle/Controller/DefaultController.php
@@ -8,16 +8,71 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 
+use Qanda\HelloBundle\Entity\Product;
+
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/{v1}/{v2}", name="_qanda_hello")
+     * @Route("/", name="product_list")
      * @Template()
      */
-    public function indexAction($v1, $v2=5)
+    public function indexAction()
     {
-        $str = $v1.$v2;
-        return array('text' => $str);
+        $products = $this->getDoctrine()
+            ->getRepository('QandaHelloBundle:Product')
+            ->findAll();
+
+        return array('products' => $products);
+    }
+
+    /**
+     * @Route("/add", name="product_add")
+     * @Template()
+     */
+    public function addAction()
+    {
+        $product = new Product();
+        $product->setName('New Product name');
+        $product->setPrice('20.99');
+        $product->setDescription('Lorem ipsum dolor sit amet');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        return array();
+    }
+
+    /**
+     * @Route("/show/{id}", name="product_show")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('QandaHelloBundle:Product')
+            ->find($id);
+
+        if (!$product){
+            throw $this->createNotFoundException("Product $id is not found!");
+        }
+
+        return array('product' => $product);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="product_delete")
+     */
+    public function deleteAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('QandaHelloBundle:Product')
+            ->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($product);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('product_list'));
     }
 }
